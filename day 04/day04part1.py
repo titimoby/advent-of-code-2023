2bf5802ac8ef69c7d702017dfc6a8eba6592eaec
Thierry Chantier <titimoby@gmail.com>
2023-12-04 16:10:39 +0100

    day04 part1 refactoring
    use of map function was a pure game changer for me here

--- day 04/day04part1.py
+++ day 04/day04part1.py
@@ -3,19 +3,11 @@ from tooling.file_access import get_content
 
 def day04part1(filename: str) -> int:
     pile_cards = get_content(filename)
-    numbers_pairs = []
     worth = 0
-    for line in pile_cards:
-        # remove card name and index, then split winning and own numbers
-        values_list = line.split(":")[1].split("|")
-        # winning numbers, numbers you have
-        winnings = [int(value) for value in values_list[0].split()]
-        own = [int(value) for value in values_list[1].split()]
-        numbers_pairs.append([winnings, own])
-    for pair in numbers_pairs:
-        matches = [value for value in pair[1] if value in pair[0]]
-        if len(matches) > 0:
-            worth += 2 ** (len(matches) - 1)
+    for card in pile_cards:
+        winnings, own = map(str.split, card.split('|'))
+        matches = set(winnings) & set(own)
+        worth += 2 ** (len(matches) - 1) if matches else 0
     return worth
 
 
